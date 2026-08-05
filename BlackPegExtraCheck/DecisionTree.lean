87630151038325d8ee6ded075f465d1745829e39
clemenskuske <33251410+clemenskuske@users.noreply.github.com>
2026-08-05 15:18:06 +0200

    Import finite product and function instances

--- BlackPegExtraCheck/DecisionTree.lean
+++ BlackPegExtraCheck/DecisionTree.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Clemens Kuske. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Clemens Kuske, OpenAI Codex
+-/
+import Mathlib.Data.Fintype.BigOperators
 import Mathlib.Data.Fintype.Perm
 
 /-!
